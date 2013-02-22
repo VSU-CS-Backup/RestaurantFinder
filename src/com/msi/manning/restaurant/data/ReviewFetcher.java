b53b16146e4ec0b3fd80a361d97d2138d272116e
Zhiguang Xu <xuzhiguang@gmail.com>
2013-02-22 18:22:55 -0500

    In the middle of using Yelp and parsing JSON.

--- src/com/msi/manning/restaurant/data/ReviewFetcher.java
+++ src/com/msi/manning/restaurant/data/ReviewFetcher.java
@@ -4,97 +4,176 @@ import android.util.Log;
 
 import com.msi.manning.restaurant.Constants;
 
-import org.xml.sax.InputSource;
-import org.xml.sax.XMLReader;
-
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
+import org.json.*;
+
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.oauth.OAuthService;
 
 /**
- * Use mock web service (based on the now-defunct Google Base) with specified criteria to obtain Review data.
+ * Use Yelp web service with specified criteria to obtain Review data.
  * 
- * @author charliecollins
+ * @author Zhiguang Xu
  */
 public class ReviewFetcher {
 
-    private static final String CLASSTAG = ReviewFetcher.class.getSimpleName();
-    private static final String QBASE = "http://navitend.com/androidinaction/reviews.php?type=restaurant";
-    private static final String QD_PREFIX = "&description=";
-    private static final String QL_PREFIX = "&location=";
-    private String query;
+	private static final String CLASSTAG = ReviewFetcher.class.getSimpleName();
+	// private static final String QBASE =
+	// "http://navitend.com/androidinaction/reviews.php?type=restaurant";
+	// private static final String QD_PREFIX = "&description=";
+	// private static final String QL_PREFIX = "&location=";
+	// private String query;
 
-    /**
-     * Construct ReviewFetcher with location, description, rating, and paging params.
-     * 
-     * @param location
-     * @param description
-     * @param rating
-     * @param start
-     * @param numResults
-     */
-    public ReviewFetcher(String loc, String description, String rat, int start, int numResults) {
+	private OAuthService service;
+	private Token accessToken;
+	private OAuthRequest request;
 
-        Log.v(Constants.LOGTAG, " " + ReviewFetcher.CLASSTAG + " location = " + loc + " rating = " + rat + " start = "
-            + start + " numResults = " + numResults);
+	private final ArrayList<Review> reviews;
 
-        String location = loc;
-        String rating = rat;
+	/**
+	 * Construct ReviewFetcher with location, description, rating, and paging
+	 * params.
+	 * 
+	 * @param location
+	 * @param description
+	 * @param rating
+	 * @param start
+	 * @param numResults
+	 */
+	public ReviewFetcher(String loc, String des, String rat, int start,
+			int numResults) {
+		// Update tokens here from Yelp developers site, Manage API access.
+		// http://www.yelp.com/developers/manage_api_keys
+		String consumerKey = "0Zn9dxuhMUyXoNB5WErI1w";
+		String consumerSecret = "nMYqpY1ZqSelxzGCT69WnoswNjU";
+		String token = "BVSsPFNspS-GWA3OmeeDreIGqK-KTFST";
+		String tokenSecret = "TijS_myGG1-EIlzNmY8LOxepETI";
 
-        // urlencode params
-        try {
-            if (location != null) {
-                location = URLEncoder.encode(location, "UTF-8");
-            }
-            if (rating != null) {
-                rating = URLEncoder.encode(rating, "UTF-8");
-            }
-        } catch (UnsupportedEncodingException e1) {
-            e1.printStackTrace();
-        }
+		service = new ServiceBuilder().provider(YelpApi2.class)
+				.apiKey(consumerKey).apiSecret(consumerSecret).build();
+		accessToken = new Token(token, tokenSecret);
 
-        // build query
-        this.query = ReviewFetcher.QBASE;
-        if ((location != null) && !location.equals("")) {
-            this.query += (ReviewFetcher.QL_PREFIX + location);
-        }
-        if ((description != null) && !description.equals("ANY")) {
-            this.query += (ReviewFetcher.QD_PREFIX + description);
-        }
+		Log.v(Constants.LOGTAG, " " + ReviewFetcher.CLASSTAG + " location = "
+				+ loc + " description = " + des + " rating = " + rat
+				+ " start = " + start + " numResults = " + numResults);
 
-        Log.v(Constants.LOGTAG, " " + ReviewFetcher.CLASSTAG + " query - " + this.query);
-    }
+		String location = loc;
+		String description = des;
+		String rating = rat;
 
-    /**
-     * Call Google Base and parse via SAX.
-     * 
-     * @return
-     */
-    public ArrayList<Review> getReviews() {
-        long startTime = System.currentTimeMillis();
-        ArrayList<Review> results = null;
+		// urlencode params
+		try {
+			if (location != null) {
+				location = URLEncoder.encode(location, "UTF-8");
+			}
+			if (description != null) {
+				description = URLEncoder.encode(description, "UTF-8");
+			}
+			if (rating != null) {
+				rating = URLEncoder.encode(rating, "UTF-8");
+			}
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		}
 
-        try {
-            URL url = new URL(this.query);
-            SAXParserFactory spf = SAXParserFactory.newInstance();
-            SAXParser sp = spf.newSAXParser();
-            XMLReader xr = sp.getXMLReader();
+		// build query
+		// this.query = ReviewFetcher.QBASE;
+		// if ((location != null) && !location.equals("")) {
+		// this.query += (ReviewFetcher.QL_PREFIX + location);
+		// }
+		// if ((description != null) && !description.equals("ANY")) {
+		// this.query += (ReviewFetcher.QD_PREFIX + description);
+		// }
 
-            ReviewHandler handler = new ReviewHandler();
-            xr.setContentHandler(handler);
+		// build Yelp request
+		String term = "Restaurant";
+		request = new OAuthRequest(Verb.GET, "http://api.yelp.com/v2/search");
+		if ((description != null) && !description.equals("ANY")) {
+			request.addQuerystringParameter("term", description + " " + term);
+		} else {
+			request.addQuerystringParameter("term", term);
+		}
+		if ((location != null) && !location.equals("")) {
+			request.addQuerystringParameter("location", location);
+		}
+		if ((rating != null) && !rating.equals("")) {
+			request.addQuerystringParameter("rating", rating);
+		}
 
-            xr.parse(new InputSource(url.openStream()));
-            // after parsed, get record
-            results = handler.getReviews();
-        } catch (Exception e) {
-            Log.e(Constants.LOGTAG, " " + ReviewFetcher.CLASSTAG, e);
-        }
-        long duration = System.currentTimeMillis() - startTime;
-        Log.v(Constants.LOGTAG, " " + ReviewFetcher.CLASSTAG + " call and parse duration - " + duration);
-        return results;
-    }
+		request.addQuerystringParameter("limit", "20");
+		service.signRequest(accessToken, request);
+
+		Log.v(Constants.LOGTAG, " " + ReviewFetcher.CLASSTAG + " request - "
+				+ this.request);
+
+		reviews = new ArrayList<Review>();
+	}
+
+	/**
+	 * Parse JSON.
+	 * 
+	 * @return
+	 */
+	private ArrayList<Review> parseReviews(String resp) {
+		try {
+			JSONObject mainJson = new JSONObject(resp);
+			JSONArray businesses = mainJson.getJSONArray("businesses");
+			for (int i = 0; i < businesses.length(); i++) {
+				JSONObject business = businesses.getJSONObject(i);
+				System.out.println("name: " + business.getString("name"));
+				System.out.println("rating: " + business.getString("rating"));
+				System.out.println("phone: " + business.getString("phone"));
+				JSONArray address = business.getJSONObject("location")
+						.getJSONArray("display_address");
+				String address_string = "";
+				for (int j = 0; j < address.length(); j++)
+					address_string = address_string + address.getString(j)
+							+ " ";
+				System.out.println("address: " + address_string);
+				System.out.println();
+				
+				Review review = new Review();
+				review.name = business.getString("name");
+				review.rating = business.getString("rating");
+				review.phone = business.getString("phone");
+				review.location = address_string;
+				reviews.add(review);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return reviews;
+	}
+
+	/**
+	 * Call Yelp.
+	 * 
+	 * @return
+	 */
+	public ArrayList<Review> getReviews() {
+		long startTime = System.currentTimeMillis();
+		ArrayList<Review> results = null;
+
+		try {
+			Response response = request.send();
+			String resp = response.getBody();
+
+			// after receiving response, get it parsed
+			results = parseReviews(resp);
+		} catch (Exception e) {
+			Log.e(Constants.LOGTAG, " " + ReviewFetcher.CLASSTAG, e);
+		}
+		long duration = System.currentTimeMillis() - startTime;
+		Log.v(Constants.LOGTAG, " " + ReviewFetcher.CLASSTAG
+				+ " send request and parse reviews duration - " + duration);
+		return results;
+	}
 }
